b63594cd8bef5ba10090fdca55c2eab73dfd0057
Michael Flaherty <michaelwflaherty@me.com>
2017-08-24 21:10:57 -0700

    php comments instead of html

--- upload.php
+++ upload.php
@@ -1,22 +1,23 @@
-<!--  Lysis Web Decompiler Wrapper
- -
- -  Copyright (C) 2017 Michael Flaherty // michaelwflaherty.com // michaelwflaherty@me.com
- - 
- - This program is free software: you can redistribute it and/or modify it
- - under the terms of the GNU General Public License as published by the Free
- - Software Foundation, either version 3 of the License, or (at your option) 
- - any later version.
- -
- - This program is distributed in the hope that it will be useful, but WITHOUT 
- - ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
- - FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- -
- - You should have received a copy of the GNU General Public License along with 
- - this program. If not, see http://www.gnu.org/licenses/.
- -
- -->
+<?php
+	/**  Lysis Web Decompiler Wrapper
+	 *
+	 *  Copyright (C) 2017 Michael Flaherty // michaelwflaherty.com // michaelwflaherty@me.com
+	 * 
+	 * This program is free software: you can redistribute it and/or modify it
+	 * under the terms of the GNU General Public License as published by the Free
+	 * Software Foundation, either version 3 of the License, or (at your option) 
+	 * any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful, but WITHOUT 
+	 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+	 * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License along with 
+	 * this program. If not, see http://www.gnu.org/licenses/.
+	 *
+	 **/
 
- <?php
+
 	$target_dir = "/home/admin/public_html/lysis/";
 	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	$type = pathinfo($target_file, PATHINFO_EXTENSION);
